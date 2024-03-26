e426bdd53a09ee3430da65769c6c7e7c0e00f711
mrogers8 <mrogers8@wisc.edu>
2024-03-26 12:13:07 -0400

    Create Game box and Title plus basic styling

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
+import './Game.css';
+
+class Title extends Component {
+  render() {
+    return (
+      <header className="Title">
+        Bubble Shooter
+      </header>
+    );
+  }
+}
 
 class Game extends Component {
   render() {
     return (
       <div className="Game" >
-        Hello World
+        <Title />
+        <div className="Border" >
+        </div>
       </div>
     );
   }
