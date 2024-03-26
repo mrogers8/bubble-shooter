73c7d426c1ba09e1933542fdb5c551761ac0723a
mrogers8 <mrogers8@wisc.edu>
2024-03-26 11:52:08 -0400

    Remove template and initialize Game component

---
+++ src/components/Game.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="Game" >
+        Hello World
+      </div>
+    );
+  }
+}
+
+export default Game
